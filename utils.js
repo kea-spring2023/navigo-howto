505e457548d605cdb3f0128296866fc937b12890
Lars Mortensen <lmor@kea.dk>
2023-03-07 16:20:55 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -75,7 +75,7 @@ export async function handleHttpErrors(res) {
   if (!res.ok) {
     const errorResponse = await res.json();
     const error = new Error(errorResponse.message)
-    error.apiError = errorResponse
+    error.fullResponse = errorResponse
     throw error
   }
   return res.json()
